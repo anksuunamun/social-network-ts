7661b8a7f7646eb878676dbad72897e470a4de4a
anksuunamun <shishkina_veronika_10@mail.ru>
2021-01-12 23:20:07 +0300

    build: add ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
+
 import HeaderContainer from './Components/Header/HeaderContainer';
 import NavbarContainer from './Components/Navbar/NavbarContainer';
 import ProfileContainer from './Components/Profile/ProfileContainer';
+
 
 function App() {
     return (
         <div className="appWrapper">
             <HeaderContainer/>
             <NavbarContainer/>
-            <ProfileContainer />
+            <Route path={"/profile"} render={ () => <ProfileContainer />}/>
+
+
         </div>
+
     );
 }
 
