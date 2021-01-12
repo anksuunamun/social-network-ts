f06ee0a2a427d8174109499609be3a757535d39c
anksuunamun <shishkina_veronika_10@mail.ru>
2021-01-13 02:50:58 +0300

    style: add styles for header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import styles from './Header.module.css';
+import socialLogo from '../../Assets/Images/socialLogo.png'
+import { NavLink } from 'react-router-dom';
 
 function Header() {
     return (
-    <div className={styles.headerWrapper}>Header</div>
+        <div className={styles.headerWrapper}>
+            <div className={styles.logo}>
+                <img src={socialLogo} alt=""/>
+            </div>
+            <div className={styles.userActionsWrapper}>
+                <div className={styles.userName}>UserName</div>
+                {<button className={styles.authButtonWrapper}>Log out</button> || <NavLink to={'/login'} className={styles.authButtonWrapper}>Log in</NavLink>}
+            </div>
+        </div>
     )
 }
 
