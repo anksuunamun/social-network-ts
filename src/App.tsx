f0d60428e8208219a9fe3ecdf957bb8e5f0213de
anksuunamun <shishkina_veronika_10@mail.ru>
2021-01-12 23:23:49 +0300

    refactor: delete antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import './App.css';
-import {Route} from 'react-router-dom';
-
 import HeaderContainer from './Components/Header/HeaderContainer';
 import NavbarContainer from './Components/Navbar/NavbarContainer';
 import ProfileContainer from './Components/Profile/ProfileContainer';
-
 
 function App() {
     return (
         <div className="appWrapper">
             <HeaderContainer/>
             <NavbarContainer/>
-            <Route path={"/profile"} render={ () => <ProfileContainer />}/>
-
-
+            <ProfileContainer />
         </div>
-
     );
 }
 
