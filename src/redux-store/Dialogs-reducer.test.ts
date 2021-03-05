c8bba4b6bde2b6b5d1037258c5ac7f7dc7930d69
anksuunamun <shishkina_veronika_10@mail.ru>
2021-03-05 20:53:54 +0300

    test: add tests for dialogs-reducer

---
+++ src/redux-store/Dialogs-reducer.test.ts
@@ -0,0 +1,34 @@
+import {v1} from 'uuid';
+import {dialogsReducer, DialogsReducerStateType} from './Dialogs-reduser';
+
+
+test('correct message should be added', () => {
+    let startState: DialogsReducerStateType = {
+        'dialogs': [],
+        'messages': [
+            {
+                'id': v1(),
+                'messageText': 'Hello!'
+            },
+            {
+                'id': v1(),
+                'messageText': 'Hey! See my new photos!'
+            },
+            {
+                'id': v1(),
+                'messageText': 'YO!!'
+            },
+            {
+                'id': v1(),
+                'messageText': 'I want some pizza...'
+            },
+        ]
+    }
+
+    let newMessageText = 'I want to add new message!!';
+    let endState = dialogsReducer(startState, addNewMessageAC(newMessageText));
+
+    expect(endState.messages.length).toBe(5);
+    expect(endState.messages[4].messageText).toBe(newMessageText);
+    expect(endState.dialogs.length).toBe(0);
+})
