d0962096c6ea0ba01da4dc0a0a76928764b38605
anksuunamun <shishkina_veronika_10@mail.ru>
2021-03-14 19:18:12 +0300

    feat: implement pagination

---
+++ src/Components/Common/Paginator/Paginator.tsx
@@ -0,0 +1,77 @@
+import React, {useEffect, useState} from 'react';
+import {v1} from 'uuid';
+import PurpleButton from '../PurpleButton/PurpleButton';
+import styles from './Paginator.module.css';
+
+type PaginatorPropsType = {
+    totalCount: number
+    currentPage: number
+    portionSize: number
+    pagesPortionSize: number
+    onPageClickHandler: (item: number) => void
+}
+
+const Paginator: React.FC<PaginatorPropsType> = (props) => {
+
+    const [portionNumber, setPortionNumber] = useState<number>(1);
+    const [leftBorder, setLeftBorder] = useState<number>((portionNumber - 1) * props.pagesPortionSize + 1);
+    const [rightBorder, setRightBorder] = useState<number>(portionNumber * props.pagesPortionSize);
+    const generatePages = () => {
+        let tempPages = []
+        for (let i = leftBorder; i <= rightBorder; i++) {
+            tempPages.push(i);
+        }
+        return tempPages;
+    }
+    const [pages, setPages] = useState<Array<number>>(generatePages());
+
+    const pagesCount = Math.ceil(props.totalCount / props.portionSize);
+
+
+    useEffect(() => {
+        let tempPages = [];
+        for (let i = leftBorder; i <= rightBorder; i++) {
+            tempPages.push(i);
+        }
+        setPages(tempPages);
+    }, [leftBorder, rightBorder])
+
+    const onPageClickHandler = (page: number) => {
+        setPortionNumber(page);
+        props.onPageClickHandler(page);
+    }
+    const onNextButtonClick = () => {
+        setLeftBorder(leftBorder + 10);
+        setRightBorder(rightBorder + 10);
+    }
+    const onBackButtonClick = () => {
+        setLeftBorder(leftBorder - 10);
+        setRightBorder(rightBorder - 10);
+    }
+
+    return (
+        <div>
+            {leftBorder !== 1
+                ? <PurpleButton text={'back'}
+                                small
+                                onButtonClick={() => {
+                                    onBackButtonClick()
+                                }}/>
+                : ''}
+            {pages.map(page => <span key={v1()}
+                                     className={`${styles.pageDigit} ${page === props.currentPage
+                                         ? styles.currentPage
+                                         : ''}`}
+                                     onClick={() => onPageClickHandler(page)}>{page}</span>)}
+            {rightBorder !== pagesCount
+                ? <PurpleButton text={'next'}
+                                small
+                                onButtonClick={() => {
+                                    onNextButtonClick()
+                                }}/>
+                : ''}
+        </div>
+    )
+}
+
+export default Paginator;
