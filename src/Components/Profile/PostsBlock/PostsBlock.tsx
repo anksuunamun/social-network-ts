3372d58cd8f8027052275779f1728f2ecaaa8444
anksuunamun <shishkina_veronika_10@mail.ru>
2021-04-12 17:43:35 +0300

    feat: add redux form for adding post

--- src/Components/Profile/PostsBlock/PostsBlock.tsx
+++ src/Components/Profile/PostsBlock/PostsBlock.tsx
@@ -1,21 +1,23 @@
-import React, {ChangeEvent, useState} from 'react';
+import React from 'react';
 import styles from './PostsBlock.module.css'
 import Post from './Post/Post';
 import {PostsType} from '../../../redux-store/Profile-reducer';
 import PurpleButton from '../../Common/PurpleButton/PurpleButton';
-import CustomTextarea from '../../Common/CustomTextarea/CustomTextarea';
 import {InjectedFormProps, reduxForm, Field} from 'redux-form';
+import {TextArea} from '../../Common/FieldControls/FieldControls';
 
 
 type OwnAddPostFormPropsType = {
     newPostText: string
 }
+
+
 let AddPostForm: React.FC<InjectedFormProps<OwnAddPostFormPropsType>> = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <Field name={'newPostText'}
-                   component={'textarea'}/>
-            <button>submit</button>
+                   component={TextArea}/>
+            <PurpleButton text={'add post'}/>
         </form>
 
     )
@@ -31,31 +33,16 @@ type PostsBlockType = {
 
 function PostsBlock(props: PostsBlockType) {
 
-    const [newPostText, setNewPostText] = useState<string>('')
-
-    const onclickHandler = () => {
-        props.addPost(newPostText)
-        setNewPostText('')
-    }
-
-    const onChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
-        setNewPostText(event.target.value)
+    const onFormSubmit = (data: OwnAddPostFormPropsType) => {
+        console.log(data)
+        props.addPost(data.newPostText);
     }
 
     return (
         <div className={styles.postsBlock}>
             <div>
                 <p>My posts</p>
-                <CustomTextarea name={'newPost'}
-                                id={'newPost'}
-                                value={newPostText}
-                                onChange={onChangeHandler}/>
-                <AddPostReduxForm onSubmit={(data) => {
-                    console.log(data)
-                }}/>
-                <div>
-                    <PurpleButton text={'Add post'} onButtonClick={onclickHandler}/>
-                </div>
+                <AddPostReduxForm onSubmit={onFormSubmit}/>
             </div>
             <div className={styles.posts}>
                 {props.posts.map(
