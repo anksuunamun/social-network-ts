47a1e7eb8ae823ea7452e0e7ef9bbed0b19689a1
anksuunamun <shishkina_veronika_10@mail.ru>
2021-04-25 17:48:59 +0300

    feat: add function for updating objects in array

---
+++ src/utils/objectHelpers.ts
@@ -0,0 +1,6 @@
+export const updateObjectInArray = (items: any, itemId: any, objPropName: any, newObjProps: any) => {
+    return items.map((item: any) => item[objPropName] === itemId
+        ? {...item, ...newObjProps}
+        : item)
+
+}
