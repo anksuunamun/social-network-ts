da039e09f9336636fbcff287a260cfadd05d3325
anksuunamun <shishkina_veronika_10@mail.ru>
2021-04-26 17:01:47 +0300

    feat: add possibility to custom button type

--- src/Components/Common/PurpleButton/PurpleButton.tsx
+++ src/Components/Common/PurpleButton/PurpleButton.tsx
@@ -9,6 +9,7 @@ type PurpleButtonPropsType = {
     className?: string
     disabled?: boolean
     navLink?: string
+    type?: 'button' | 'submit' | 'reset' | undefined
 }
 
 const PurpleButton: React.FC<PurpleButtonPropsType> = (props: PurpleButtonPropsType) => {
@@ -16,7 +17,8 @@ const PurpleButton: React.FC<PurpleButtonPropsType> = (props: PurpleButtonPropsT
         <>
             <button className={`${styles.buttonWrapper} ${props.small ? styles.small : ''} ${props.className}`}
                     onClick={props.onButtonClick}
-                    disabled={props.disabled}>
+                    disabled={props.disabled}
+                    type={props.type ? props.type : 'button'}>
                 {props.navLink ? <NavLink to={props.navLink}>{props.text}</NavLink> : props.text}
             </button>
         </>
